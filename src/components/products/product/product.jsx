9633fc7854411347570cc322d24ddfa2ab9c9255
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-04 21:43:01 +0600

    addad buy product count

--- src/components/products/product/product.jsx
+++ src/components/products/product/product.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./product.css";
-const Product = ({ product }) => {
-  console.log(product);
+const Product = ({ product, handelBuyProducts }) => {
+  //   console.log(product);
   return (
     <div className="card">
       <h3>category: {product.category} </h3>
       <img className="w" src={product.image} alt="" />
       <p>price: ${product.price} </p>
+      <button onClick={handelBuyProducts}>buy</button>
     </div>
   );
 };
--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -1,17 +1,30 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Product from "./product/product";
 import "./products.css";
 
 const Products = ({ productsPromise }) => {
   const products = use(productsPromise);
   console.log(products);
+
+  const [card, setCard] = useState(0);
+
+  const handelBuyProducts = () => {
+    // console.log("click to add", product);
+    const addCard = card + 1;
+    setCard(addCard);
+  };
+
   return (
     <div>
       <h3>All Products is here {products.length}</h3>
-
+      <h3>buy products: {card}</h3>
       <div className="cardProduct">
         {products.map((product) => (
-          <Product product={product}></Product>
+          <Product
+            key={product.id}
+            handelBuyProducts={handelBuyProducts}
+            product={product}
+          ></Product>
         ))}
       </div>
     </div>
