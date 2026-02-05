4109378971483dde07a6d77603f4036e70f28b93
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-05 22:50:00 +0600

    addad fetch another away

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ const productsPromise = fetch("https://fakestoreapi.com/products").then((res) =>
   res.json(),
 );
 
+// const products = async () => {
+//   const res = await fetch("https://fakestoreapi.com/products");
+//   return res.json();
+// };
+
 function App() {
+  // const productsPromise = products();
   return (
     <>
       <Suspense fallback={<h3>products data are loading...</h3>}>
